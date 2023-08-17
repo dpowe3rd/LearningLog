67a63fb2da57f4368eef4a18a7a9167da415c518
Darrell S. Powe III <Dpowe3rd@gmail.com>
2023-08-17 11:03:59 -0400

    Fixed logout system
    Corrected the logout function by changing to Djangos 'logout' module instead of 'LogoutView' module

--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import logout
 
 # Create your views here.
 
 
 def logout_view(request):
     """Log the user out"""
-    LogoutView(request)
+    logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
