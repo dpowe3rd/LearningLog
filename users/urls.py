f74cb0dc23f86fcf094fa827810d071289be1f84
Darrell Powe <dpowe3rd@gmail.com>
2023-09-06 11:22:08 -0400

    backwards

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,13 @@
 """Defines URL patterns for users"""
 
 from django.contrib.auth.views import LoginView
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # Include default auth urls
-    path('', include('django.contrib.auth.urls')),
+
     # Login Page
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     # Log out page
