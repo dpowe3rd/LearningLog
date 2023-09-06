2031623e2797520a9bbea97d665dc2b9bde53b33
Darrell Powe <dpowe3rd@gmail.com>
2023-09-06 10:47:14 -0400

    v

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -30,6 +30,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required()
 def topic(request, topic_id):
     """Show a single topic and all of its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -41,6 +42,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required()
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':  # No data submitted; Create a blank form.
@@ -57,6 +59,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required()
 def new_entry(request, topic_id):
     """Add a new entry for a selected topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -78,6 +81,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required()
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
@@ -99,6 +103,7 @@ def edit_entry(request, entry_id):
     return render(request, 'learning_logs/edit_entry.html', context)
 
 
+@login_required()
 def delete_entry(request, entry_id):
     """Delete a selected/existing entry"""
     entry = Entry.objects.get(id=entry_id)
