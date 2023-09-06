37908144588f9745679ce26bba87cf79864d2301
Darrell Powe <dpowe3rd@gmail.com>
2023-09-06 10:56:36 -0400

    d

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -24,8 +24,9 @@ def register(request):
 
         if form.is_valid():
             new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
             # Login the new user and redirect to homepage
-            login(request, new_user)
+            login(request, )
             return HttpResponseRedirect(reverse('learning_logs:index'))
 
     context = {'form': form}
