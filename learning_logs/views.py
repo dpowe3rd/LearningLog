a22771f751ebb01b4dbe6b29785b1c533aac0148
Darrell Powe <dpowe3rd@gmail.com>
2023-09-06 10:31:32 -0400

    needed a decorator

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,6 +22,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required()
 def topics(request):
     """Shows us all the topics."""
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
